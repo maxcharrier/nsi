d52b12f261e59312514f98f0e6ca22848089fc86
Max Charrier <maxcharrier42@gmail.com>
2023-04-04 07:45:53 +0000

    queue and stack

---
+++ terminale/graphes/algorithmes/activite_1.py
@@ -0,0 +1,34 @@
+# FILE
+file = [5, 10, 20, 40]
+
+file.insert(0, 2)
+print(file)
+
+print(file.pop())
+print(file)
+
+file = [5, 10, 20, 40]
+
+file.append(2)
+print(file)
+
+print(file.pop(0))
+print(file)
+
+
+# PILE
+pile = [5, 10, 20, 40]
+
+pile.insert(0, 2)
+print(pile)
+
+print(pile.pop(0))
+print(pile)
+
+pile = [5, 10, 20, 40]
+
+pile.append(2)
+print(pile)
+
+print(pile.pop())
+print(pile)
