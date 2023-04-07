c4a4eafeda60cdd987d5f254897d0f23494b5f2d
Max Charrier <maxcharrier42@gmail.com>
2023-04-07 07:23:08 +0000

    graph using matrix

---
+++ terminale/graphes/algorithmes/graphe_matrice.py
@@ -0,0 +1,37 @@
+def creer_graphe(sommets: list[str]):
+    """ Créer un graphe en utilisant une matrice d'adjacence """
+    dimension = len(sommets)
+    matrice = [[None for _ in range(dimension)] for _ in range(dimension)]
+    return (sommets, matrice)
+
+
+def ajouter_arete(
+        graphe: tuple[list[str], list[list[None]]],
+        sommet1: str,
+        sommet2: str,
+        poids: int | list[int]
+    ):
+    """ Ajouter une arête (connexion entre deux sommets) au graphe """
+    sommets = graphe[0]
+    matrice = graphe[1]
+    i = sommets.index(sommet1)
+    j = sommets.index(sommet2)
+    matrice[i][j] = poids
+    return graphe
+
+
+def tab_sommets(graphe: tuple[list[str], list[list[None]]]):
+    """ Renvoie le tableau des sommets """
+    return graphe[0]
+
+
+def voisins(graphe: tuple[list[str], list[list[None]]], sommet: str):
+    """ Afficher les voisins d'un sommet """
+    sommets = graphe[0]
+    matrice = graphe[1]
+    i = sommets.index(sommet)
+    tab_voisins = []
+    for j in range(len(matrice[i])):
+        if matrice[i][j] is not None:
+            tab_voisins.append(sommets[j])
+    return tab_voisins
